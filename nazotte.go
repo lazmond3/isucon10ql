a6c9ac128d42f798e297ee257b1d559bd90b076c
ryok <ryok@example.com>
2020-09-12 16:33:17 +0900

    minor

--- nazotte.go
+++ nazotte.go
@@ -44,7 +44,9 @@ func searchEstateNazotte(c echo.Context) error {
 		validatedEstate := Estate{}
 
 		point := fmt.Sprintf("'POINT(%f %f)'", estate.Latitude, estate.Longitude)
-		query := fmt.Sprintf(`SELECT * FROM estate WHERE id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s))`, coordinates.coordinatesToText(), point)
+		query := fmt.Sprintf(`SELECT * FROM estate WHERE id = ? AND ` +
+			` ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s))`,
+			coordinates.coordinatesToText(), point)
 		err = db.Get(&validatedEstate, query, estate.ID)
 		if err != nil {
 			if err == sql.ErrNoRows {
