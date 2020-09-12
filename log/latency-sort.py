ecda641328d7385b85862a2cdc1cea724b768f98
Your Name <you@example.com>
2020-09-12 09:30:12 +0000

    update modifypy

--- log/latency-sort.py
+++ log/latency-sort.py
@@ -1,8 +1,9 @@
 import re
 import json
 
-fname = "bench2-after-key.log"
-late = "latency.dic.log"
+# fname = "bench2-after-key.log"
+fname = "file2.log"
+
 
 start = '{"time":'
 counter = 0
@@ -20,15 +21,12 @@ with open(fname) as f:
         ll.append(jso)
     
     ll2 = sorted(ll, key = lambda x: -x["latency"] if "latency" in x else 0)
-    with open(late, "w") as ff:
-        for ob in ll2:
-            st = json.dumps(ob) + "\n"
-            ff.write(st)
+    
     with open("latency.csv", "w") as ff2:
         for ob in ll2:
             new_ob = {}
             if not "latency" in ob: continue
-            ff2.write(ob["method"] + "," + ob["uri"] + "," + ob["user_agent"] + "," + str(ob["latency"]) + "\n")
+            ff2.write(ob["method"] + "," + ob["uri"] + ","  + str(ob["latency"]) + "\n")
 
 
 
